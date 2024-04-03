84a85866ae4f07bb062835b556acd4605006f1fe
Facundo <fnriquelme7@gmail.com>
2024-04-03 19:30:05 -0300

    validaciones formulario

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -63,10 +63,10 @@ export const Checkout = () => {
                             theme: "dark"
                         })
                     })
-                })
-                emptyCart();
-                e.target.reset();
-                navigate("/")
+            })
+        emptyCart();
+        e.target.reset();
+        navigate("/")
     }
 
     return (
@@ -74,14 +74,73 @@ export const Checkout = () => {
             {
                 carrito.length == 0 ?
                     <>
-                    <div className="min-h-[95vh] py-28 flex flex-col justify-center items-center">
-                        <h2 className="text-4xl text-gray-600 mb-16">Para finalizar la compra debe tener productos en el carrito!</h2>
-                        <Link to={"/"}>
-                            <button className="flex justify-center font-Montserrat text-sm bg-[#D90429] hover:bg-[#EF233C] rounded-lg  border-black px-3 py-3 text-white font-medium">Volver al Inicio</button>
-                        </Link>
-                    </div>
+                        <div className="min-h-[95vh] py-28 flex flex-col justify-center items-center">
+                            <h2 className="text-4xl text-gray-600 mb-16">Para finalizar la compra debe tener productos en el carrito!</h2>
+                            <Link to={"/"}>
+                                <button className="flex justify-center font-Montserrat text-sm bg-[#D90429] hover:bg-[#EF233C] rounded-lg  border-black px-3 py-3 text-white font-medium">Volver al Inicio</button>
+                            </Link>
+                        </div>
                     </>
                     :
+                    <div className="min-h-[95vh] py-28 grid grid-cols-3">
+                        <div className="p-4 py-0 col-span-3">
+                            <form id="checkout" className="flex place-items-start w-full px-10 py-4 " ref={formRef} onSubmit={handleSubmit}>
+                                <div className="w-2/3 p-5 bg-[#e0e2e6] rounded-xl">
+                                    <h2 className="font-semibold text-xl py-2">Checkout</h2>
+                                    <div className="mb-10">
+                                        <h3 className="font-semibold text-lg pt-2 pb-5" >Info de Pago</h3>
+                                        <div className="grid grid-cols-4 gap-x-2 gap-y-8 px-4">
+                                            <input placeholder="Nombre" title="Ingrese un Nombre valido" pattern="^[A-Za-z]+(?:\s+[A-Za-z]+)?$" required className="border-2 border-gray-300 rounded-lg p-2" type="text" name="nombre" />
+                                            <input placeholder="Apellido" pattern="[A-Za-z]{2,}" required className="border-2 border-gray-300 rounded-lg p-2" type="text" name="apellido" />
+                                            <input placeholder="Ciudad" pattern="[A-Za-z\s?]{2,}" required className="border-2 border-gray-300 rounded-lg col-start-3 col-span-2 p-2" type="text" name="ciudad" />
+                                            <input placeholder="Direccion de Facturacion" pattern="^[A-Za-z]+\s+(?:[A-Za-z]+\s+)*\d+$" required className="border-2 border-gray-300 rounded-lg  col-span-2 p-2" type="text" name="direccionFacturacion" />
+                                            <input placeholder="Codigo Postal" pattern="^[0-9]{4}$" required className="border-2 border-gray-300 rounded-lg p-2 "maxLength="4" type="text" name="cp" />
+                                            <input placeholder="Numero de Telefono" pattern="^[0-9]{10}$" required className="border-2 border-gray-300 rounded-lg p-2 col-start-1 col-span-2" maxLength="10" type="tel" name="tel" />
+                                        </div>
+                                    </div>
+                                    <div className="mb-10">
+                                        <h3 className="font-semibold text-lg pt-2 pb-5" >Metodo de Pago</h3>
+                                        <div className="grid grid-cols-4 gap-x-2 gap-y-8 px-4">
+                                            <select className="border-2 col-span-2 border-gray-300 rounded-lg p-2" name="tarteja" id="">
+                                                <option value="visa">Visa</option>
+                                                <option value="master-card">Master Card</option>
+                                            </select>
+                                            <input type="text" pattern="[0-9]{16}" autoComplete="cc-number" maxLength="16" placeholder="xxxx xxxx xxxx xxxx" required className=" border-2 border-gray-300 rounded-lg p-2 col-start-1 col-span-2" name="tarjetaNumero" />
+                                            <input placeholder="Codigo Seguridad" pattern="[0-9]{3}" required className="border-2 border-gray-300 rounded-lg p-2" type="text" maxLength="3" name="tarjetaCodigo" />
+                                            <input placeholder="Mes vencimiento" pattern="[0-9]{2}" required className="border-2 border-gray-300 rounded-lg p-2 col-start-1 " maxLength="2" type="text" name="mesExp" />
+                                            <input placeholder="Año vencimiento" pattern="[0-9]{2}" required className="border-2 border-gray-300 rounded-lg p-2 "  inputMode="text" type="text" maxLength="2" name="yearExp" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="p-4 font-Montserrat fixed right-10">
+                            <div className="w-full h-full col-span-3 border-2 rounded-md flex justify-center items-center border-gray-200 ">
+                                <p className="p-8 font-alata font-medium text-2xl ">Resumen de la Compra</p>
+                            </div>
+                            <div className="col-span-3 flex flex-col justify-center items-start p-8 border-2 border-y-0  border-gray-200">
+                                <p className="font-bold text-lg">Total - ${totalPrice()}</p>
+                                <p className="font-medium pt-2 text-sm">El total no incluye envio ni impuestos*</p>
+                            </div>
+                            <div className="col-span-3 p-3 border-2 border-t-0 border-gray-200">
+                                <div>
+                                    <input type="submit" form="checkout" className="w-full p-3 text-white font-semibold bg-[#D90429] hover:bg-[#EF233C]" />
+                                </div>
+                                <div>
+                                    <button onClick={emptyCart} className=" w-full p-3 text-black font-semibold bg-[#e0e2e6] hover:bg-[#EF233C]">Vaciar Carrito</button>
+                                </div>
+                                <div>
+                                    <Link to={"/"}>
+                                        <button className="w-full p-3 text-black  font-semibold bg-[#e0e2e6] hover:bg-[#EF233C]">Seguir Comprando</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            }
+        </>)
+}
+/*
                     <div className="px-10 py-28 bg-[#e0e2e6]">
                         <form className="flex place-items-start w-full px-10 py-4 " ref={formRef} onSubmit={handleSubmit}>
                             <div className="w-2/3 p-5 bg-white">
@@ -122,8 +181,5 @@ export const Checkout = () => {
                             </div>
                         </form>
                     </div>
-            }
-        </>)
-}
-
+*/
 
